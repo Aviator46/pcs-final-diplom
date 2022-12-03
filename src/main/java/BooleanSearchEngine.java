96f5ce93e4f1bc5ddbb67c7276d096b806cb9c53
Evgeny Nikishov <evgenynikishov@mail.ru>
2022-12-03 23:06:25 +0300

    BooleanSearchEngine completed

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,12 +1,47 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BooleanSearchEngine implements SearchEngine {
-    private Map<String, List<PageEntry>> everyWordMap = new HashMap<>();
+    private Map<String, List<PageEntry>> wordMap = new HashMap<>(); //индексация
+    private File pdfsDir;
+
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
+        File[] pdfsList = pdfsDir.listFiles(); //массив с файлами для поиска
+        if (pdfsList == null) {
+            throw new IOException("Файлы для поиска отсутствуют");
+        } else {
+            for (File pdf : pdfsList) { //перебираем все файлы
+                var doc = new PdfDocument(new PdfReader(pdf)); //работаем с файлом
+                for (int page = 1; page < doc.getNumberOfPages(); page++) { //перебираем все страницы
+                    var text = PdfTextExtractor.getTextFromPage(doc.getPage(page)); //получаем текст со страницы
+                    var words = text.split("\\P{IsAlphabetic}+"); //разбиваем текст на слова
 
+                    Map<String, Integer> freqs = new HashMap<>(); //считаем частоту слов
+                    for (var word : words) {
+                        if (word.isEmpty()) {
+                            continue;
+                        }
+                        word = word.toLowerCase();
+                        freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                    }
+                    for (Map.Entry<String, Integer> entry : freqs.entrySet()) {
+                        if (wordMap.containsKey(entry.getKey())) {
+                            wordMap.get(entry.getKey()).add(new PageEntry(pdf.getName(), page, entry.getValue()));
+                        } else {
+                            wordMap.put(entry.getKey(), new ArrayList<>());
+                            wordMap.get(entry.getKey()).add(new PageEntry(pdf.getName(), page, entry.getValue()));
+                        }
+                    }
+                }
+            }
+        }
         // прочтите тут все pdf и сохраните нужные данные,
         // тк во время поиска сервер не должен уже читать файлы
     }
@@ -14,15 +49,13 @@ public class BooleanSearchEngine implements SearchEngine {
     @Override
     public List<PageEntry> search(String word) {
         // тут реализуйте поиск по слову
-        return Collections.emptyList();
-    }
-
-    Map<String, Integer> freqs = new HashMap<>(); // мапа, где ключом будет слово, а значением - частота
-    for(var word :words) {// перебираем слова
-        if (word.isEmpty()) {
-            continue;
+        List<PageEntry> result = null;
+        if (wordMap.containsKey(word)) {
+            result = wordMap.get(word)
+                    .stream()
+                    .sorted(Comparator.comparing(PageEntry::getCount))
+                    .collect(Collectors.toList());
         }
-        word = word.toLowerCase();
-        freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+        return result;
     }
 }
